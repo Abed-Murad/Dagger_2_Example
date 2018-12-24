4c5b65b357ef1fb5d0f3a4fcab344d6271d7f49c
Abed Murad <abdallah.Murad@mail.com>
2018-12-24 15:13:50 +0100

    Add @Inject to our classes

--- app/src/main/java/com/am/dagger_2_example/CarComponent.java
+++ app/src/main/java/com/am/dagger_2_example/CarComponent.java
@@ -1,5 +1,14 @@
 package com.am.dagger_2_example;
 
+import dagger.Component;
+/**
+* Directed Acyclic Graph (DAG_ger)
+* Directed means one direction
+* Acyclic means it does not have cycles, no Class have an instance of it self
+*/
+@Component
 public interface CarComponent {
+
+    Car getCar();
 
 }
