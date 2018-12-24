52b90d7f9a4183c5ba017486e03ff868e20c9286
Abed Murad <abdallah.Murad@mail.com>
2018-12-24 15:32:50 +0100

    Finish Part 3

--- app/src/main/java/com/am/dagger_2_example/CarComponent.java
+++ app/src/main/java/com/am/dagger_2_example/CarComponent.java
@@ -1,14 +1,16 @@
 package com.am.dagger_2_example;
 
 import dagger.Component;
+
 /**
-* Directed Acyclic Graph (DAG_ger)
-* Directed means one direction
-* Acyclic means it does not have cycles, no Class have an instance of it self
-*/
+ * Directed Acyclic Graph (DAG_ger)
+ * Directed means one direction
+ * Acyclic means it does not have cycles, no Class have an instance of it self
+ */
 @Component
 public interface CarComponent {
 
     Car getCar();
 
+    void inject(MainActivity mainActivity);
 }
--- app/src/main/java/com/am/dagger_2_example/MainActivity.java
+++ app/src/main/java/com/am/dagger_2_example/MainActivity.java
@@ -3,17 +3,19 @@ package com.am.dagger_2_example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         CarComponent component = DaggerCarComponent.create();
-
-        Car car = component.getCar();
-
+        component.inject(this);
+        
         car.drive();
     }
 }
